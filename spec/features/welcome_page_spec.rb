91dd023b3bf78b43c7c735ac3a433ca4a3596eeb
Madis Nõmme <madis.nomme@gmail.com>
2014-08-16 19:33:45 +0300

    Create test for welcome page content

--- spec/features/welcome_page_spec.rb
+++ spec/features/welcome_page_spec.rb
@@ -1,6 +1,9 @@
 feature 'Pages' do
-  it 'has homepage' do
+  let(:post) { Post.new title: 'The truth is in the code', content: 'Code mode'}
+
+  it 'homepage shows content' do
     visit '/'
-    expect(page.status_code).to be 200
+    expect(page).to have_content(post.content)
+    expect(page).to have_title(post.title)
   end
 end
