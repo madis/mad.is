37812f391eebff1e6e04d95225c4565ac6942b3a
Madis Nõmme <madis.nomme@gmail.com>
2014-08-19 00:02:09 +0300

    Remove placeholder image from demo post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
   def index
-    @posts = [Post.new(title: 'Ja ja ja', content: IO.read(Rails.root + 'README.md'), updated_at: Time.now)]
+    @posts = [Post.new(title: 'First post', content: IO.read(Rails.root + 'README.md'), updated_at: Time.now)]
   end
 end
